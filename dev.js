7558326f00e128858ff30217b91c91a03f8a4c91
continuouselectronics <will.senisi@gmail.com>
2020-01-08 13:36:34 -0500

    updated to 0.1.0

--- dev.js
+++ dev.js
@@ -7,50 +7,44 @@ import {
   reduce
 } from "./lib/util";
 
-export default function() {
-  return "test";
+export default function updateState(prevState, updater) {
+  if (isObjLiteral(prevState) && isObjLiteral(updater)) {
+    const nextState = reduce(
+      function(acc, updaterVal, key) {
+        acc[key] = updateState(prevState[key], updaterVal);
+
+        return acc;
+      },
+      updater,
+      {}
+    );
+
+    return { ...prevState, ...nextState };
+  }
+  if (isArray(prevState) && isArray(updater)) {
+    return [...prevState, ...updater];
+  }
+  if (isFunction(updater)) {
+    return updater(getArg(prevState));
+  }
+  return updater;
 }
 
-export function named() {
-  return "named";
+function mapWithUpdater(fn, objOrArr) {
+  return map(function(val, keyOrIdx) {
+    return updateState(val, fn(val, keyOrIdx));
+  }, objOrArr);
 }
-// export default function updateState(prevState, updater) {
-//   const mapWithUpdater = function(fn, objOrArr) {
-//     return map(function(val, keyOrIdx) {
-//       return updateState(val, fn(val, keyOrIdx));
-//     }, objOrArr);
-//   };
-//   const getArg = function(prevState) {
-//     const fnArg =
-//       isArray(prevState) || isObjLiteral(prevState) ? prevState : [prevState];
 
-//     return {
-//       prevState,
-//       map: f => map(f, fnArg),
-//       filter: f => filter(f, fnArg),
-//       reduce: (f, init) => reduce(f, fnArg, init),
-//       mapWithUpdater: (f, init) => mapWithUpdater(f, fnArg, init)
-//     };
-//   };
+function getArg(prevState) {
+  const fnArg =
+    isArray(prevState) || isObjLiteral(prevState) ? prevState : [prevState];
 
-//   if (isObjLiteral(prevState) && isObjLiteral(updater)) {
-//     const nextState = reduce(
-//       function(acc, updaterVal, key) {
-//         acc[key] = updateState(prevState[key], updaterVal);
-
-//         return acc;
-//       },
-//       updater,
-//       {}
-//     );
-
-//     return { ...prevState, ...nextState };
-//   }
-//   if (isArray(prevState) && isArray(updater)) {
-//     return [...prevState, ...updater];
-//   }
-//   if (isFunction(updater)) {
-//     return updater(getArg(prevState));
-//   }
-//   return updater;
-// }
+  return {
+    prevState,
+    map: f => map(f, fnArg),
+    filter: f => filter(f, fnArg),
+    reduce: (f, init) => reduce(f, fnArg, init),
+    mapWithUpdater: (f, init) => mapWithUpdater(f, fnArg, init)
+  };
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require("path");
-const common = require("./webpack");
-
+console.log("o");
 module.exports = {
   entry: "./dev.js",
   output: {
     filename: "build.js",
-    path: path.resolve(__dirname)
+    path: path.resolve(__dirname),
+    libraryTarget: "commonjs2"
   },
   mode: "production",
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: "node_modules",
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: ["babel-loader"]
       }
     ]
